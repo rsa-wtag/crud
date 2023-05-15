e94fe1f54b529033a69a26c36b4ad8622bc13218
Rumman Sadik <rumman.sadik@welldev.io>
2023-05-15 17:00:28 +0600

    Create controller & view and setup routes

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,21 @@
+class TasksController < ApplicationController
+  def index
+    # Index
+  end
+
+  def show
+    # show
+  end
+
+  def new
+    # new
+  end
+
+  def create
+    # create
+  end
+
+  def destroy
+    # destroy
+  end
+end
